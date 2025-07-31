316244a01f158d338080ac9942b5c056dd710547
watarikai <watarikai@outlook.com>
2025-07-31 21:23:53 +0900

    Rebranded to COUNT

---
+++ app/src/main/java/com/count/COUNTActivity.kt
@@ -0,0 +1,27 @@
+package com.count
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.count.ui.theme.QuietCraftTheme
+
+class COUNTActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuietCraftTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    // Main screens composable(s) go here!
+                    COUNTScreen(
+
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/count/ui/screen/COUNTScreen.kt
@@ -0,0 +1,2 @@
+package com.count.ui.screen
+
